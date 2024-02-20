3c905f51e0ba3a8e1b961b3b87c5748e33ef24c5
hammadAuthornate <hammad@authornate.com>
2024-02-20 16:35:25 +0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-
 export default function Footer() {
   return (
     <>
-      <footer className="mt-20 bg-neutral-100/70 text-center text-neutral-600 ">
+      <footer className="mt-20 bg-neutral-100/10 text-center text-white ">
         <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6  lg:justify-between">
-          <div className="mr-12 hidden md:block">
+          <div className="mr-12 hidden md:block text-white">
             <span>Get connected with us:</span>
           </div>
           <div className="flex justify-center items-center">
-            <a href="#!" className="mr-6 text-neutral-600 ">
+            <a href="#!" className="mr-6 text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -19,7 +17,7 @@ export default function Footer() {
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
               </svg>
             </a>
-            <a href="#!" className="mr-6 text-neutral-600 ">
+            <a href="#!" className="mr-6 text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -29,7 +27,7 @@ export default function Footer() {
                 <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
             </a>
-            <a href="#!" className="mr-6 text-neutral-600 ">
+            <a href="#!" className="mr-6 text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -43,7 +41,7 @@ export default function Footer() {
                 />
               </svg>
             </a>
-            <a href="#!" className="mr-6 text-neutral-600 ">
+            <a href="#!" className="mr-6 text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -53,7 +51,7 @@ export default function Footer() {
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
               </svg>
             </a>
-            <a href="#!" className="mr-6 text-neutral-600 ">
+            <a href="#!" className="mr-6 text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -63,7 +61,7 @@ export default function Footer() {
                 <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
               </svg>
             </a>
-            <a href="#!" className="text-neutral-600 ">
+            <a href="#!" className="text-white ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -76,46 +74,20 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="mx-6 pt-8 pb-4 text-center">
-          <div className="relative mb-7">
-            <form action="">
-              <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-5 mb-6">
-                <div className="md:ml-auto">
-                  <p className="font-bold">Sign up for our newsletter</p>
-                </div>
-
-                <div>
-                  <label className="sr-only">Small input</label>
-                  <input
-                    type="email"
-                    id="inputSignUp"
-                    placeholder="Your address"
-                    className="w-full block rounded-lg border py-[9px] px-3 pr-4 text-sm focus:border-blue-400 focus:ring-1 focus:ring-blue-400 focus:outline-none"
-                  />
-                </div>
-
-                <div className="md:mr-auto">
-                  <button
-                    type="submit"
-                    className="inline-block rounded bg-indigo-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-indigo-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-indigo-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-indigo-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-
         <div className="bg-transparent p-6 text-center  flex justify-center items-center">
           <span>© 2023 Copyright: </span>
 
           <a className="mx-3" href="#">
-            <img src="/logo.png" className="h-5" alt="TE Logo" loading="lazy" />
+            <img
+              src="/logo.png"
+              className="size-10 invert"
+              alt="Logo"
+              loading="lazy"
+            />
           </a>
 
           <a
-            className="font-semibold text-neutral-600 "
+            className="font-semibold text-white "
             href="https://tw-elements.com/"
           >
             Chizu
