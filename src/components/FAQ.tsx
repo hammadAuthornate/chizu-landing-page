1d619d9703334ca122ef63a92060d851f8e958ab
hammadAuthornate <hammad@authornate.com>
2024-02-22 10:36:59 +0500

    refactored FAQ section

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function FAQ() {
-  const [openid, setOpenId] = useState<string | null>("accordion-open-body-1");
+  const [selectedId, setSelectedId] = useState<number | null>(null);
   function HelpIcon() {
     return (
       <svg
@@ -18,11 +18,11 @@ export default function FAQ() {
       </svg>
     );
   }
-  function DropDownIcon({ id }: { id: string }) {
+  function DropDownIcon({ id }: { id: number }) {
     return (
       <svg
         data-accordion-icon
-        className={`w-3 h-3 ${openid === id ? "" : "rotate-180"} shrink-0`}
+        className={`w-3 h-3 ${selectedId === id ? "" : "rotate-180"} shrink-0`}
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -39,102 +39,72 @@ export default function FAQ() {
     );
   }
 
+  const FAQs = [
+    {
+      title: "What is Chizu?",
+      description:
+        "Chizu is a token on the ether network that can mint NFTs on the blockchain.",
+      id: 1,
+    },
+    {
+      title: "How can i mint for free?",
+      description:
+        "If you have the Founders Pass OR the Executive Founders Pass, Chizu can allow you to mint NFTs for free for a specific number of times at a reduced minting price.",
+      id: 2,
+    },
+    {
+      title: "What are the minting charges?",
+      description:
+        "The Standard Minting charges are 0.003 Eth, but for Users that have Founders pass or the Executive Founders Pass, they have the reduced minitng price of 0.001 ETH.",
+      id: 3,
+    },
+  ];
+
   return (
     <div className="mt-20 flex flex-col items-center">
       <div className="max-w-xl">
         <div className="my-8 font-extrabold text-4xl text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-violet-400">
           Frequently Asked Questions
         </div>
-        <div id="accordion-open" data-accordion="open" className="">
-          <h2 id="accordion-open-heading-1">
-            <button
-              onClick={() => setOpenId("accordion-open-body-1")}
-              type="button"
-              className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200  hover:bg-gray-100/20  gap-3"
-              data-accordion-target="#accordion-open-body-1"
-              aria-expanded="true"
-              aria-controls="accordion-open-body-1"
-            >
-              <span className="flex items-center">
-                <HelpIcon /> What is Chizu?
-              </span>
-              <DropDownIcon id="accordion-open-body-1" />
-            </button>
-          </h2>
-          {openid === "accordion-open-body-1" && (
-            <div
-              id="accordion-open-body-1"
-              //   className="hidden"
-              aria-labelledby="accordion-open-heading-1"
-            >
-              <div className="p-5 border border-b-0 border-gray-200 ">
-                <p className="mb-2 text-gray-200 ">
-                  Chizu is a token on the ether network that can mint NFTs on
-                  the blockchain.
-                </p>
+        <div id="accordion-open" data-accordion="open">
+          <div>
+            {FAQs.map((faq, index) => (
+              <div key={index}>
+                <h2 id={faq.id.toString()}>
+                  <button
+                    onClick={() => {
+                      selectedId === faq.id
+                        ? setSelectedId(null)
+                        : setSelectedId(faq.id);
+                    }}
+                    type="button"
+                    className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 border  border-gray-200 rounded-xl focus:ring-4 focus:ring-gray-200  hover:bg-gray-100/20  gap-3"
+                    data-accordion-target={faq.id.toString()}
+                    aria-expanded="true"
+                    aria-controls={faq.id.toString()}
+                  >
+                    <span className="flex items-center">
+                      <HelpIcon /> {faq.title}
+                    </span>
+                    <DropDownIcon id={faq.id} />
+                  </button>
+                </h2>
+                {selectedId === faq.id && (
+                  <div
+                    id={faq.id.toString()}
+                    className="my-4"
+                    aria-labelledby={faq.id.toString()}
+                  >
+                    <div className="p-5 border rounded-md border-gray-200 ">
+                      <p className="mb-2 text-gray-200 text-center">
+                        {faq.description}
+                      </p>
+                    </div>
+                  </div>
+                )}
               </div>
-            </div>
-          )}
-
-          <h2 id="accordion-open-heading-2">
-            <button
-              onClick={() => setOpenId("accordion-open-body-2")}
-              type="button"
-              className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200  hover:bg-gray-100/20 gap-3"
-              data-accordion-target="#accordion-open-body-2"
-              aria-expanded="false"
-              aria-controls="accordion-open-body-2"
-            >
-              <span className="flex items-center">
-                <HelpIcon />
-                How can i mint for free?
-              </span>
-              <DropDownIcon id="accordion-open-body-2" />
-            </button>
-          </h2>
-          {openid === "accordion-open-body-2" && (
-            <div
-              id="accordion-open-body-2"
-              aria-labelledby="accordion-open-heading-2"
-            >
-              <div className="p-5 border border-b-0 border-gray-200 ">
-                <p className="mb-2 text-gray-200 ">
-                  If you have the Founders Pass OR the Executive Founders Pass,
-                  Chizu can allow you to mint NFTs for free for a specific
-                  number of times at a reduced minting price.
-                </p>
-              </div>
-            </div>
-          )}
-          <h2 id="accordion-open-heading-3">
-            <button
-              onClick={() => setOpenId("accordion-open-body-3")}
-              type="button"
-              className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 border border-gray-200 focus:ring-4 focus:ring-gray-200  hover:bg-gray-100/20  gap-3"
-              data-accordion-target="#accordion-open-body-3"
-              aria-expanded="false"
-              aria-controls="accordion-open-body-3"
-            >
-              <span className="flex items-center">
-                <HelpIcon /> What are the minting charges?
-              </span>
-              <DropDownIcon id="accordion-open-body-3" />
-            </button>
-          </h2>
-          {openid === "accordion-open-body-3" && (
-            <div
-              id="accordion-open-body-3"
-              aria-labelledby="accordion-open-heading-3"
-            >
-              <div className="p-5 border border-t-0 border-gray-200 ">
-                <p className="mb-2 text-gray-200 ">
-                  The Standard Minting charges are 0.003 Eth, but for Users that
-                  have Founders pass or the Executive Founders Pass, they have
-                  the reduced minitng price of 0.001 ETH.
-                </p>
-              </div>
-            </div>
-          )}
+            ))}
+          </div>
         </div>
       </div>
     </div>
